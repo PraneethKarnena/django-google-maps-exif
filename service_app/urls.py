e9e9f6859b6a1f146fa2d2957993a18270fac924
Praneeth <praneeth.codes@gmail.com>
2019-09-05 01:05:04 +0530

    added route for Dashboard page

--- service_app/urls.py
+++ service_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home_view, name='home_page'), # route for Home page
     path('register/', views.registration_view, name='registration_page'), # route for Registration page
     path('login/', views.login_view, name='login_page'), # route for Login page
+    path('dashboard/', views.dashboard_view, name='dashboard_page'),
 ]
