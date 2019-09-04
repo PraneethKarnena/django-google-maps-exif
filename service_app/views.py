00b77d677670f42fcc45b8fcc81d03a60562cf6a
Praneeth <praneeth.codes@gmail.com>
2019-09-05 00:02:11 +0530

    improved readability

--- service_app/views.py
+++ service_app/views.py
@@ -4,4 +4,8 @@ from django.views.decorators.http import require_http_methods
 
 @require_http_methods(['GET'])
 def home_view(request):
+    """
+    Logic for Home page
+    Return a simple Home template
+    """
     return render(request, 'service/home.html')
