c53e1ade04e2f615472c9783775accf65b9e79d1
Praneeth <praneeth.codes@gmail.com>
2019-09-05 00:01:18 +0530

    restricted HTTP action to GET

--- service_app/views.py
+++ service_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
 
+@require_http_methods(['GET'])
 def home_view(request):
     return render(request, 'service/home.html')
