d901297f71fef7490f60d0ff6287ccd031855905
Praneeth <praneeth.codes@gmail.com>
2019-09-05 00:38:32 +0530

    added route for Login page

--- service_app/urls.py
+++ service_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home_view, name='home_page'), # route for Home page
     path('register/', views.registration_view, name='registration_page'), # route for Registration page
+    path('login/', views.login_view, name='login_page'), # route for Login page
 ]
