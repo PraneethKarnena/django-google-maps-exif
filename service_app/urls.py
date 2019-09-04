3c9975e28d5edd89afc524e17c81454580b31ed0
Praneeth <praneeth.codes@gmail.com>
2019-09-05 00:10:13 +0530

    added route for Registration page

--- service_app/urls.py
+++ service_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home_page'), # route for Home page
+    path('register/', views.registration_view, name='registration_page'), # route for Registration page
 ]
