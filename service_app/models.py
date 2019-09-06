47bdf3957a906db6da0cbb39f329d13d9e9489e3
Praneeth <praneeth.codes@gmail.com>
2019-09-06 07:34:23 +0530

    added Models for Images and Jobs

--- service_app/models.py
+++ service_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ImageModel(models.Model):
+    """
+    Stores Images, and their EXIF data
+    """
+    image = models.ImageField(verbose_name='Image', help_text='Upload an Image')
+
+    IMAGE_TYPE_CHOICES = (
+        ('SRC', 'Source'),
+        ('DST', 'Destination'),
+    )
+    image_type = models.CharField(max_length=3, choices=IMAGE_TYPE_CHOICES, null=False, blank=False)
+
+    def __str__(self):
+        return f'{str(self.image)} - {self.image_type}'
+
+
+class JobModel(models.Model):
+    """
+    Image processing job model
+    Process images to extract EXIF data
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    images = models.ManyToManyField(ImageModel)
+
+    JOB_STATUS_CHOICES = (
+        ('PRS', 'Processing'),
+        ('COM', 'Complete'),
+        ('ERR', 'Error'),
+    )
+    status = models.CharField(max_length=3, choices=JOB_STATUS_CHOICES, default='PRS')
+
+    def __str__(self):
+        return f'{str(self.user)} - {self.status}'
