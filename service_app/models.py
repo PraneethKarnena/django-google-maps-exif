e04d94697771739cf6a2f478d2050456ea78fd46
Praneeth <praneeth.codes@gmail.com>
2019-09-07 08:30:56 +0530

    added error column on Job table

--- service_app/models.py
+++ service_app/models.py
@@ -48,6 +48,7 @@ class JobModel(models.Model):
         ('ERR', 'Error'),
     )
     status = models.CharField(max_length=3, choices=JOB_STATUS_CHOICES, default='PRS')
+    errors = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.user.username
