85a3dbf0b0d19728397bec1ca4e24220424cf2e7
Praneeth <praneeth.codes@gmail.com>
2019-09-07 22:28:23 +0530

    configured route for media files

--- distance_matrix/urls.py
+++ distance_matrix/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 # from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include(('service_app.urls', 'service_app'), namespace='service')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
