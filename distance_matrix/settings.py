2e54b2d629d3067edc36560d24bc87a0ec6dd33f
Praneeth <praneeth.codes@gmail.com>
2019-09-07 09:27:24 +0530

    added MEDIA settings

--- distance_matrix/settings.py
+++ distance_matrix/settings.py
@@ -131,3 +131,7 @@ STATIC_URL = '/static/'
 
 LOGIN_URL = 'service_app:login_page'
 LOGOUT_REDIRECT_URL = 'service_app:login_page'
+
+# For handling uploaded files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
