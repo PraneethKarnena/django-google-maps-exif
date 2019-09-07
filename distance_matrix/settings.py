f9c3114719c227cb8f9772f7d39d5875b2e00751
Praneeth <praneeth.codes@gmail.com>
2019-09-08 04:13:00 +0530

    configured Heroku

--- distance_matrix/settings.py
+++ distance_matrix/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -135,3 +136,6 @@ LOGOUT_REDIRECT_URL = 'service_app:login_page'
 # For handling uploaded files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
